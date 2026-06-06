4c14a36e44725a9c7c34fb22062cbff06c1bac9e
Ankit Vishwakarma <ankvish1102@gmail.com>
2026-06-06 23:22:35 +0530

    build navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,10 +9,12 @@ import Collection from "./pages/Collection";
 import Product from "./pages/Product";
 import Login from "./pages/Login";
 import PlaceOrder from "./pages/PlaceOrder";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { assets } from "../assets/assets";
+import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+
+const Navbar = () => {
+  const [visible, setVisible] = React.useState(false);
+
+
+  return (
+    <div className="flex justify-between items-center py-5 font-medium">
+      <img src={assets.logo} alt="Logo" className="w-36" />
+      <ul className="hidden sm:flex gap-5 text-sm text-grey-700">
+        <NavLink to="/" className="flex flex-col items-center gap-1">
+          <p>HOME</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+        </NavLink>
+        <NavLink to="/collection" className="flex flex-col items-center gap-1">
+          <p>COLLECTION</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+        </NavLink>
+        <NavLink to="/about" className="flex flex-col items-center gap-1">
+          <p>ABOUT</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+        </NavLink>
+        <NavLink to="/contact" className="flex flex-col items-center gap-1">
+          <p>CONTACT</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+        </NavLink>
+      </ul>
+      <div className="flex items-center gap-6">
+        <img
+          src={assets.search_icon}
+          alt="Search"
+          className="w-5 cursor-pointer"
+        />
+
+        <div className=" group relative">
+          <img
+            src={assets.profile_icon}
+            alt="Cart"
+            className="w-5 cursor-pointer"
+          />
+          
+          <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
+            <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
+              <p className="cursor-pointer hover:text-black">My Profile</p>
+              <p className="cursor-pointer hover:text-black">Orders</p>
+              <p className="cursor-pointer hover:text-black">Logout</p>
+            </div>
+          </div>
+        </div>
+             <Link to="/cart" className="relative">
+              <img src={assets.cart_icon} alt="" className="w-5 min-w-5" />
+              <p className="absolute  -right-1.25 -bottom-1.25 w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px] ">0</p>
+              {/* <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center">0</span> */}
+              </Link>
+              <img onClick={() => setVisible(true)} rc={assets.menu_icon}  className="w-5 cursor-pointe sm:hidden" alt="" />
+        
+      </div>
+      {/* sidebar menu for small screens */}
+      <div className="">
+        
+      </div>
+    </div>
+  );
+};
+export default Navbar
