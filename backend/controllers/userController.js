3d290b910c739109be88cc146892f9711a8e66d0
Ankit Vishwakarma <ankvish1102@gmail.com>
2026-06-17 10:57:41 +0530

    created authentication for admin

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -88,6 +88,28 @@ const registerUser = async (req, res) => {
 // Route for Admin Login
 const adminLogin = async (req, res) => {
 
+  try {
+
+    const { email, password } = req.body;
+
+    if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
+      const token = jwt.sign(email + password, process.env.JWT_SECRET);
+      return res.json({ success: true, token, message: "Admin logged in successfully" })
+
+    } else {
+      res.json({ success: false, message: "Invalid credentials" })
+
+    }
+
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message })
+
+
+  }
+
+
+
 }
 
 
