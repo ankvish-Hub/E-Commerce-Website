36f9bd52778c4b894f773c234c9c07d1c6b36dbd
Ankit Vishwakarma <ankvish1102@gmail.com>
2026-06-18 19:31:35 +0530

    Connected backend APIs with admin panel

--- admin/src/components/Login.jsx
+++ admin/src/components/Login.jsx
@@ -1,15 +1,27 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { backendUrl } from "../App";
+import { toast } from "react-toastify";
 
-const Login = () => {
+const Login = ({setToken}) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const onSubmitHandler = async (e) => {
     try {
       e.preventDefault();
-      
+      const response = await axios.post(backendUrl + "/api/user/admin", {
+        email,
+        password,
+      });
+      if (response.data.success) {
+        setToken(response.data.token);
+      } else {
+        toast.error(response.data.message);
+      }
     } catch (error) {
-
+      console.log(error);
+      toast.error("Something went wrong");
     }
   };
 
@@ -22,7 +34,9 @@ const Login = () => {
             <p className="text-sm font-medium text-gray700 mb-2">
               Email Address
             </p>
-            <input  onChange={(e) => setEmail(e.target.value)} value={email}
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
               className="rounded-md w-full px-3 py-2 border border-gray-300"
               type="text"
               placeholder="your@email.com"
@@ -31,7 +45,9 @@ const Login = () => {
           </div>
           <div className="mb-3 min-w-72">
             <p className="text-sm font-medium text-gray700 mb-2">Password</p>
-            <input onChange={(e) => setPassword(e.target.value)} value={password}
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
               className="rounded-md w-full px-3 py-2 border border-gray-300"
               type="password"
               placeholder="Enter Your Password"
