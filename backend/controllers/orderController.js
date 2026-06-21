12266ccdc58c64f1afee1b80b466fb98ea331761
Ankit Vishwakarma <ankvish1102@gmail.com>
2026-06-21 13:10:09 +0530

    created place order feature

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,84 @@
+import orderModel from "../models/orderModel.js"
+import userModel from "../models/userModel.js"
+
+// PLacing order using COD method
+const placeOrder = async (req, res) => {
+  try {
+    const { userId, items, amount, address } = req.body;
+
+    const orderData = {
+      userId,
+      items,
+      address,
+      amount,
+      paymentMethod: "COD",
+      payment: false,
+      date: Date.now()
+    }
+
+    const newOrder = new orderModel(orderData)
+    await newOrder.save()
+
+    await userModel.findByIdAndUpdate(userId, { cartData: {} })
+
+    res.json({ success: true, message: "Order Placed" })
+
+
+
+
+
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message })
+
+  }
+
+
+
+}
+
+// PLacing order using Stripe method
+const placeOrderStripe = async (req, res) => {
+
+
+}
+
+// PLacing order using Razorpay method
+const placeOrderRazorpay = async (req, res) => {
+
+
+}
+
+// All order date for admin panel
+const allOrders = async (req, res) => {
+
+
+}
+
+// User order data for frontend
+const userOrders = async (req, res) => {
+  try {
+
+    const { userId } = req.body;
+
+    const orders = await orderModel.find({ userId })
+    res.json({ success: true, orders })
+
+
+
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.messsage })
+
+  }
+
+
+}
+
+//Update order status from admin panel
+const updateStatus = async (req, res) => {
+
+
+}
+
+export { placeOrder, placeOrderStripe, placeOrderRazorpay, allOrders, userOrders, updateStatus }
